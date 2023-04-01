3b2c8dfae43caf76efe0cda8ca9bc8bf0c5ed65f
voidiz <voidqt@hotmail.com>
2023-04-01 13:29:59 +0900

    feat: add box grid component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { extend } from "@react-three/fiber";
+import {
+  BoxGeometry,
+  MeshStandardMaterial,
+  Mesh,
+  Group,
+  DirectionalLight,
+  AmbientLight,
+} from "three";
+
+// Necessary for framer-motion-3d?
+extend({
+  BoxGeometry,
+  MeshStandardMaterial,
+  Mesh,
+  Group,
+  DirectionalLight,
+  AmbientLight,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
