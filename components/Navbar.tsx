4a1641b920adb224b3e2cb2cb75ba0d25980888b
Yaman <yamix2000@gmail.com>
2023-04-02 05:42:12 +0200

    feat: Add scroll to Hero section in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import { useCallback, useState } from "react";
 import React from "react";
 
 type NavbarProps = {
+  heroRef: React.RefObject<HTMLDivElement>;
   introRef: React.RefObject<HTMLDivElement>;
   specsRef: React.RefObject<HTMLDivElement>;
   footerRef: React.RefObject<HTMLDivElement>;
@@ -30,13 +31,17 @@ function Button({
   );
 }
 
-function Navbar({ introRef, specsRef, footerRef }: NavbarProps) {
+function Navbar({ heroRef, introRef, specsRef, footerRef }: NavbarProps) {
   const { scrollYProgress } = useScroll();
   const [scrolledUp, setScrolledUp] = useState(true);
 
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     setScrolledUp(latest <= scrollYProgress.getPrevious());
   });
+
+  const handleLogoClick = () => {
+    heroRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }
 
   return (
     <motion.div
@@ -50,7 +55,7 @@ function Navbar({ introRef, specsRef, footerRef }: NavbarProps) {
       className="fixed top-8 w-full z-50"
     >
       <div className="flex justify-between h-full mx-auto p-8 8xl:rounded-lg bg-slate-800/40 backdrop-blur-sm max-w-8xl">
-        <Image src="/img/logo-white.svg" alt="Logo" width={96} height={28} />
+        <Image src="/img/logo-3-white.svg" alt="Logo" width={96} height={28} onClick={handleLogoClick} />
         <div className="border-l-2 border-slate-600 pl-8 flex gap-8">
           <Button text="info" linkRef={introRef} />
           <Button text="specs" linkRef={specsRef} />
